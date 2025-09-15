dd22d7c9ba97de10cda09942ea8d8a5e6e3ef262
Pablo Gallo <gallopabloj@gmail.com>
2025-09-15 20:21:57 -0300

    validaciones desde el cliente

--- js/pages/recorrido_form.js
+++ js/pages/recorrido_form.js
@@ -1,4 +1,22 @@
 
 export function recorrido_form() {
-    console.log('Recorrido Form')
+    const date = document.getElementById('fecha')
+    date.addEventListener('change', (e) => {
+        const obj = new Date()
+        const inputDate = new Date(e.target.value)
+        if (obj > inputDate) {
+            e.target.setCustomValidity('Fecha de vuelo debe ser mayor a la actual')
+            e.target.reportValidity()
+        } else e.target.setCustomValidity('')
+    })
+
+    const origen = document.getElementById('origen')
+    const destino = document.getElementById('destino')
+    destino.addEventListener('change', (e) => {
+        if (destino.value === origen.value 
+           && origen.value !== undefined) {
+            e.target.setCustomValidity('Ingresar un origen y destino distinto')
+            e.target.reportValidity()
+        }
+    })
 }
