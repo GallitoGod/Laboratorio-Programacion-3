f7f4f14e8def193e6391fdc14d6e32a3607394ee
Pablo Gallo <gallopabloj@gmail.com>
2025-10-23 20:45:26 -0300

    bloque de body_id para correcto uso de js

--- avisos/static/js/index.js
+++ avisos/static/js/index.js
@@ -3,9 +3,8 @@ import { recorrido_form } from "./pages/recorrido_form.js"
 import { reserva_form } from "./pages/reserva_form.js"
 
 document.addEventListener('DOMContentLoaded', () => {
-    const page = document.body.dataset.page
 
-    if (page === 'landing_page') landing_page()
-    if (page === 'recorrido_form') recorrido_form()
-    if (page === 'reserva_form') reserva_form()
+    if (document.getElementById('landing_page')) landing_page()
+    if (document.getElementById('recorrido_form')) recorrido_form()
+    if (document.getElementById('reserva_form')) reserva_form()
 })
