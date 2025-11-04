133bf3cbaa898c9519dca8522c187fd3826f8c87
lautaro arce <lautaroarce345@gmail.com>
2025-11-04 19:48:25 -0300

    mas cambios 2

--- colectivos/views.py
+++ colectivos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from colectivos.models import Colectivo
+from usuarios.models import Usuario
 
-# Create your views here.
+def formulario_colectivo(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        cantAsientos = request.POST.get('cantAsientos')
+        matricula = request.POST.get('matricula')
+        descripcion = request.POST.get('descripcion')
+        operador = request.POST.get('operador')
+        Colectivo.objects.create(nombre=nombre, cantAsientos=cantAsientos, matricula=matricula, operador=operador, descripcion=descripcion, estado='Activo')
+        return render(request, 'colectivo_form.html')
+    else:
+        usuarios = Usuario.objects.all()
+        return render(request, 'colectivo.form.html', {'usuarios': usuarios})
+
+
