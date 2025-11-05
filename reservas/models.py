cc3d88a41d4a15dba841e4f2df649b59916f4ca7
Pablo Gallo <gallopabloj@gmail.com>
2025-11-05 15:06:43 -0300

    cambio en reservas

--- reservas/models.py
+++ reservas/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from recorridos.models import Circuito
 from colectivos.models import Colectivo
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Reserva(models.Model):
+    usuario = models.ForeignKey(User, on_delete= models.CASCADE, default= None, null= True)
     fecha = models.DateField()
     puntoPartida = models.CharField(max_length= 100)
     cantCupos = models.IntegerField()
